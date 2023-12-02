46a3a5f230c1fb8aae25ac630086dd8eb7eb19d1
DischargedRobot <kolyaportov@yandex.ru>
2023-12-02 23:49:39 +0300

    расчёт очков, расширение условий окончания, устранение бага, из-за которого нельзя было выбрать существ в следующем ходу, если брал в предыдущем

--- Game.py
+++ Game.py
@@ -7,23 +7,22 @@ class Game:
         self.cup = Cup()
         self.table = Table()
         self.all_table = PrettyTable()
-        self.flag = 0
         self.creatures_flag = {'1': 0, '2': 0, '3': 0, '4': 0}
+        self.flag = 0
     def aside(self, action):
         ''' Откладывание кубиков в строну '''
         def crows():
             self.table.plate['корова'] = self.table.dice_on_table['корова']
             self.cup.numbers -= self.table.dice_on_table['корова']
-            self.flag += 1
 
         def chikens():
             self.table.plate['курица'] = self.table.dice_on_table['курица']
             self.cup.numbers -= self.table.dice_on_table['курица']
-            self.flag += 1
+
         def people():
             self.table.plate['человек'] = self.table.dice_on_table['человек']
             self.cup.numbers -= self.table.dice_on_table['человек']
-            self.flag += 1
+
         def lasers():
             self.table.plate['лазер'] += self.table.dice_on_table['лазер']
             self.cup.numbers -= self.table.dice_on_table['лазер']
@@ -31,15 +30,20 @@ class Game:
         creatures_func = {'1': people, '2': chikens, '3': crows, '4': lasers} # для вызова функции
         creatures_str = {'1': "человек", '2': "курица", '3': "корова", '4': "лазер"} # для обращения к элементам стола
 
-        # про верка на то, брали ли мы уже существо/лазер себе на корабль
-        if self.table.dice_on_table[creatures_str[action]] != 0 and self.creatures_flag[action] == 0:
-            if action != "4":
-                for key in list(self.creatures_flag.keys())[:-1]:  #заполняем все флаги существ, так как в 1 ход можем только 1 вид похитить
-                    self.creatures_flag[key] = 1
-            else:  #заполняем флаги лазера, если пользователь рели взять их
+        # проверка на то, брали ли мы уже существо/лазер себе на корабль в этом ходу
+        if self.table.dice_on_table[creatures_str[action]] != 0 and self.creatures_flag[action] == 0 :
+            if action != "4" and self.flag == 0: # если выбрали существ
+                self.creatures_flag[action] = 1
+                self.flag = 1 #больше брать в этом ходу существ нельзя
+                creatures_func[action]()
+                return True
+            elif action == '4':  #если выбрали лазер
                 self.creatures_flag['4'] = 1
-            creatures_func[action]()
-            return True
+                creatures_func[action]()
+                return True
+            else:  # если попытались выбрать существ из другой группы
+                print("К сожалению, мы не можем это отложить, попробуйте снова")
+                return False
         else:
             print("К сожалению, мы не можем это отложить, попробуйте снова")
             return False
@@ -53,6 +57,14 @@ class Game:
                                   [self.table.dice_on_table[key] for key in self.table.dice_on_table.keys()])
         self.all_table.add_column("Уже отложены", [self.table.plate[key] for key in self.table.plate.keys()])
         print(self.all_table)
+
+
+    def score(self):
+        ''' подсчёт очков '''
+        score = self.table.plate['курица'] + self.table.plate['человек'] + self.table.plate['корова']
+        if self.table.plate['курица'] != 0 and  self.table.plate['человек'] != 0 and self.table.plate['корова'] != 0:
+            return score + 3
+        return score
 
     def step(self, action: 0):
         ''' Откладываем танки'''
@@ -70,6 +82,8 @@ class Game:
                     break
                 self.aside(flag)
                 self.paint_all_table()
+            self.creatures_flag['4'] = 0 #обнуление флагов для следующего хода
+            self.flag = 0
         elif action == '2': #просмотр содержимого стола
             print(self.all_table)
         else:
